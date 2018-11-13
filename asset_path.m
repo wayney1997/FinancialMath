c9676c6d18843f2030b1efd83ff6fb517a096b16
wayney1997 <30312913+wayney1997@users.noreply.github.com>
2018-11-13 16:11:45 +0900

    Create asset_path.m
    simple asset path simulation

---
+++ asset_path.m
@@ -0,0 +1,27 @@
+load 'data.txt';
+d = diff(log(data));
+
+drift=mean(d)+0.5*var(d)*var(d);
+vol=var(d);
+S0=19.03;
+
+T=2;
+dt=0.01;
+t=0:dt:T;
+sz=size(t,2);
+
+
+S=zeros(sz,1);
+
+figure; hold on;
+for n=1:100
+    
+    S(1)=S0;
+    xi=randn(sz,1);
+    for i=1:sz-1
+        S(i+1)=S(i)*exp( (drift-vol^2/2)*dt+ vol*sqrt(dt)*xi(i));
+    end
+    
+    plot(t,S)
+
+end
