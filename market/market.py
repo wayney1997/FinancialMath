02e0a411be2abf9fe63f426c9c69d98aebce817c
unknown <wayney1997@gmail.com>
2018-11-17 23:52:06 +0900

    Update market.py
    save results as csv, will implement histogram subplot of dlogS distribution

--- market/market.py
+++ market/market.py
@@ -1,14 +1,21 @@
 import numpy as np
+import pandas as pd
 import pandas_datareader.data as web
 import matplotlib.pyplot as plt
 import datetime, argparse
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument("symbol",help="NASDAQ ticker symbol")
 parser.add_argument("T",type=int,help="length of total time interval")
 parser.add_argument("dt",type=float,help="time interval to iterate")
 parser.add_argument("N",type=int,help="number of sample paths")
+parser.add_argument("-s","--save",action="store_true",help="save simulation data as csv")
 args = parser.parse_args()
+
+if args.save:
+    savelist = []
+    print("saving ",args.N," simulation results")
 
 title = 'NASDAQ:'+ args.symbol
 
@@ -31,7 +38,12 @@ for n in range(1,args.N):
     x=np.random.normal(0,1,len(t))
     for i in range(0,len(t)-1):
         stock[i+1]=stock[i]*np.exp(drift*args.dt+vol*np.sqrt(args.dt)*x[i])
+    savelist.append(stock)
     plt.plot(t,stock)
+
+if args.save:
+    df = pd.DataFrame(savelist)
+    df.to_csv("test.csv")
 
 plt.title(title)
 plt.xlabel('Day(s) after current time')
